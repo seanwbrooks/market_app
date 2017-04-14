6041c8db339fe34268e24b105621e5a5ee7fbb88
Sean <swbrooks87@gmail.com>
2017-04-14 09:15:55 -0400

    Finished static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def welcome
-    render html: "Investics Banatics."
-    stock = StockQuote::Stock.quote("TSLA, ACBFF")
-  end
-
 end
